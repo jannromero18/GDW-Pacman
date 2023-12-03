32222cd70a845b1ed9de50ff3c547ec51070becb
jannromero <janndenzellromero@gmail.com>
2023-12-02 19:32:02 -0500

    Power up added

--- Pacman/Assets/Scripts/PowerUp.cs
+++ Pacman/Assets/Scripts/PowerUp.cs
@@ -7,6 +7,7 @@ public class PowerUp : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D other){ //box collider isTrigger 
         if(other.transform.tag == "Player"){
             Player.canShoot = true;
+            Player._currentAmmo += 3;
             Destroy(gameObject); 
         }
     } 
